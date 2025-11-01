09516030665bc6398536788836bb812aaeea6f7e
ramizMahmmud <mahmmudramiz@gmail.com>
2025-11-01 10:57:40 +0600

    basic flask code written for adding sql

---
+++ basics/7_sql_setup.py
@@ -0,0 +1,44 @@
+from flask import Flask, redirect, session, render_template, url_for, request, flash
+from datetime import timedelta
+
+
+app = Flask(__name__)
+
+app.secret_key = b"sdihfauie"
+
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+@app.route("/user", methods = ["POST", "GET"])
+def user():
+    email = None
+    if "user" in session:
+        user = session["user"]
+        if request.method == "POST":
+            email = request.form["email"]
+            session["email"] = email
+        else:
+            if "email" in session:
+                email = session["email"]
+        return render_template("user.html",  email = email)
+   
+    else:
+        return redirect(url_for("login"))
+@app.route("/login", methods = ["POST", "GET"])
+def login():
+    if request.method == "POST":
+        user = request.form["nm"]
+        session["user"] = user
+        return redirect(url_for("user", user = user))
+    else:
+        return render_template("login.html")
+
+@app.route("/logout")
+def logout():
+    session.pop("user", None)
+    return redirect(url_for("login"))
+
+if __name__ == "__main__":
+    app.run(debug=True)
